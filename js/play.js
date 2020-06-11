5608146c217b07eefc5812a908e83c9275d58b9b
Damon <TranDamon@outlook.com>
2020-06-11 15:15:55 +0100

    Changed grid to 2dArray

--- js/play.js
+++ js/play.js
@@ -1,7 +1,10 @@
 let tiles = [
-
+  [, ,],
+  [, ,],
+  [, ,],
 ];
 
+let indexH,indexV=0;
 let height = 200;
 let width = 200;
 let canvasWidth = 900;
@@ -27,12 +30,12 @@ playerList.push(new Player(Tile_States.CROSS, color(255,0,0)));
   createCanvas(900, 900);
   // Pick colors randomly
 
-for(let loop=1; loop<=3;loop++){
-  for(let row= 1; row <=3; row++){ //constructor(Xcord,Ycord,length,states,color)
-    tiles.push(new Tile(row*200,loop*200,width, Tile_States.EMPTY,color(200), color(255)));
+for(let loop=0; loop<3;loop++){
+  for(let row= 0; row <3; row++){ //constructor(Xcord,Ycord,length,states,color)
+    tiles[loop][row] = new Tile(row*200,loop*200,width, Tile_States.EMPTY,color(200), color(255));
   }
 }
-
+console.log(tiles);
 }
 
 function get_random_state()
@@ -55,11 +58,12 @@ function get_random_state()
 }
 function draw() {
 //Iterate throughout the tile array and draw them
-  for(let i = 0; i<tiles.length;i++){
-    tiles[i].drawMe();
+
+  for(let loop=0; loop<3;loop++){
+    for(let row= 0; row <tiles[loop].length; row++){ //constructor(Xcord,Ycord,length,states,color)
+      tiles[loop][row].drawMe();
+    }
   }
-
-
 
 }
 
@@ -68,20 +72,25 @@ function mousePressed() {
   // Check if mouse is inside the circle
 
 let currentPlayer = playerList[(turnCount % 2)];
+  for(let i = 0; i < tiles.length; i++){
+    for(let j = 0; j<tiles[i].length;j++){
+      if (tiles[i][j].isInBounds(mouseX, mouseY)) {
 
-  for(let i = 0; i<tiles.length;i++){
-    if (tiles[i].isInBounds(mouseX, mouseY)) {
-
-      if (tiles[i].canInsertSymbol())
-      {
-      tiles[i].updateColour(currentPlayer.getColor());
-      tiles[i].updateState(currentPlayer.getSymbol());
-      console.log(tiles[i].getState());
-      //need logic to make sure they haven't dont do an illegal move before incrementing
-      turnCount++;
+        if (tiles[i][j].canInsertSymbol())
+        {
+          indexH = i;
+          indexV= j;
+        tiles[i][j].updateColour(currentPlayer.getColor());
+        tiles[i][j].updateState(currentPlayer.getSymbol());
+        console.log(tiles[i][j].getState());
+        //need logic to make sure they haven't dont do an illegal move before incrementing
+        turnCount++;
+        }
+      }
     }
-  }
-  }
+}
+console.log(indexH);
+console.log(indexV);
 
 }
 
@@ -89,6 +98,10 @@ function checkTilesForWinCon()
 {
   //iterate throughout tiles to check winning patterns
 
+  //Find the current tile that was just mouseClicked
+  //Then search wincon from that tile. We check to see the type of tile. i.e. if its a corner tile, you know win con is row,diagonal and column.
+  //If tile is a middle one then check row and columns
+  //If tile is actual centre piece, check row,column,diagnols.
 
-  
+
 }
