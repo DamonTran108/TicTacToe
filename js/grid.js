652cf16c1071492dc0b021aa14182dcbbe36e386
JoshuaRastall27 <Joshua_Rastall@aol.co.uk>
2020-06-11 18:54:33 +0100

    Refactored code to be encapsulated in the grid class, also made grid more dynamic

--- js/grid.js
+++ js/grid.js
@@ -1,8 +1,23 @@
 let emptyArray = [,,,];
 class Grid{
-  constructor(xCord,yCord, rowLength, colLength){
+  //WHEN DOING STUFF WITH THE ROWS AND COLUMNS
+  //FIRST DO THE COLUMN ITERATION
+  //THEN DO THE ROW ITERATION
+
+
+  //Example
+  //  for(let loop=0; loop<this.cLength;loop++)
+  //  {
+  //    for(let row= 0; row <this.rLength; row++)
+  //    {
+  //    this.grid[loop][row].drawMe();
+  //    }
+  //  }
+
+  constructor(xCord,yCord,i_t_length, rowLength, colLength){
     this.x = xCord;
     this.y = yCord;
+    this.t_length = i_t_length;
 
     this.rLength = rowLength;
     this.cLength = colLength;
@@ -31,13 +46,13 @@ class Grid{
     //Populate row with tiles
     //Push
 
-    for(let loop=0; loop<this.rLength;loop++){
+    for(let loop=0; loop<this.cLength;loop++){
 
       let tempArray = [];
 
       //populate new temp array with tiles
-      for(let row= 0; row <this.cLength; row++){ //constructor(Xcord,Ycord,length,states,color)
-        tempArray.push(new Tile(row*200,loop*200,width, Tile_States.EMPTY,color(200), color(255)));
+      for(let row= 0; row <this.rLength; row++){ //constructor(Xcord,Ycord,length,states,color)
+        tempArray.push(new Tile((row*this.t_length +this.x) ,(loop*this.t_length+this.y),this.t_length, Tile_States.EMPTY,color(200), color(255)));
       }
        //push newly populated array into grid 2d array
       this.grid.push(tempArray);
@@ -48,13 +63,43 @@ class Grid{
   {
   return this.grid;
   }
+
   drawGrid()
   {
     for(let loop=0; loop<this.cLength;loop++){
-      for(let row= 0; row <this.rLength; row++){ //constructor(Xcord,Ycord,length,states,color)
-      this.grid[loop][row].drawMe();
+       //tile constructor(Xcord,Ycord,length,states,color)
+      for(let row= 0; row <this.rLength; row++){
+        this.grid[loop][row].drawMe();
       }
     }
 
   }
+
+
+  checkGridBounds(i_mouseX, i_mouseY, i_player)
+  {
+    for(let i = 0; i < this.getColLength(); i++)
+    {
+      for(let j = 0; j< this.getRowLength();j++)
+      {
+        if (this.getGrid()[i][j].isInBounds(i_mouseX, i_mouseY))  //If is on one of the tiles
+        {
+          if (this.getGrid()[i][j].canInsertSymbol()) //check if they can insert the symbol
+          {
+
+            this.getGrid()[i][j].updateColour(i_player.getColor());
+            this.getGrid()[i][j].updateState(i_player.getSymbol());
+            //breaks out of the loop straight away as soon as it's found
+            //Prevents algorithm from searching through other itles when the required on is already found
+            return true;
+          }
+        }
+      }
+  }
+
+  //Click doesn't find any tiles so does not do any processes
+  return false;
+
+  }
+
 }
--- js/play.js
+++ js/play.js
@@ -1,10 +1,10 @@
-let tiles = [
-  [, ,],
-  [, ,],
-  [, ,],
-];
 
-let indexH,indexV=0;
+//Coded by Damon Tran and Joshua Rastall
+//Simple tic tac toe game
+//Simple concepts but is made to be modular so certain areas of code can be refactored and reused for other programs
+//Large amount of control over the grid and stuff (4x4 or 5x5 grid)
+//But is not effective for tictactoe as it is based around around a 3x3 grid
+
 let height = 200;
 let width = 200;
 let canvasWidth = 900;
@@ -12,7 +12,6 @@ let canvasHeight = 900;
 
 //player functionality
 let playerList = [];
-let player = null;
 
 //need to add 1 on the turns for the display
 //starts from 0 so it works with the array
@@ -20,7 +19,8 @@ let turnCount = 0;
 let grid = null;
 
 function setup() {
-grid = new Grid(200,200, 3, 3);
+grid = new Grid(100,52,200, 4, 3);
+//Create the players with their colours and chosen tiles
 playerList.push(new Player(Tile_States.NAUGHT, color(0,0,255)));
 playerList.push(new Player(Tile_States.CROSS, color(255,0,0)));
 
@@ -34,39 +34,21 @@ console.log(grid);
 function draw() {
 //Iterate throughout the tile array and draw them
 
-  grid.drawGrid()
+  grid.drawGrid();
 
 }
 
 // When the user clicks the mouse
 function mousePressed() {
-  // Check if mouse is inside the circle
-
 
 //remove tile length and put into grid as they'll all be the same
-
 let currentPlayer = playerList[(turnCount % 2)];
 
-// checkGridBounds(int mousex, int mousey)
-  for(let i = 0; i < grid.getColLength(); i++){
-    for(let j = 0; j<grid.getRowLength();j++){
-      if (grid.getGrid()[i][j].isInBounds(mouseX, mouseY)) {
-
-        if (grid.getGrid()[i][j].canInsertSymbol())
-        {
-          indexH = i;
-          indexV= j;
-      grid.getGrid()[i][j].updateColour(currentPlayer.getColor());
-      grid.getGrid()[i][j].updateState(currentPlayer.getSymbol());
-      console.log("State : "+ grid.getGrid()[i][j].getState());
-        //need logic to make sure they haven't dont do an illegal move before incrementing
-        turnCount++;
-        }
-      }
-    }
-}
-console.log(indexH);
-console.log(indexV);
+// Pass in mouse data to check if a tile is clicked
+//Pass in currentPlayer so it can simply update the tile as soon as it finds interval
+//Would use pointers but javascript
+if (grid.checkGridBounds(mouseX,mouseY,currentPlayer))
+{turnCount++;}
 
 }
 
