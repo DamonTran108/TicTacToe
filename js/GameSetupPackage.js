e790e7446fa19f7065c1db7b2145eed6ff32ba80
Damon <TranDamon@outlook.com>
2020-06-23 16:40:36 +0100

    Added arguments to constructor for setup

--- js/GameSetupPackage.js
+++ js/GameSetupPackage.js
@@ -1,14 +1,14 @@
 class GameSetupPackage
 {
 
-  constructor()
+  constructor(player1, player2, matchCount, roundTimer)
   {
     //players data
-    this.player1 = null;
-    this.player2 = null;
+    this.player1 = player1;
+    this.player2 = player2;
 
-    this.matchCount = 0; //1,3,5,7,9,11
-    this.roundTimer = 0; //if 0 just ignore
+    this.matchCount = matchCount; //1,3,5,7,9,11
+    this.roundTimer = roundTimer; //if 0 just ignore
 
     this.tilesToWin = 3;
     this.xtiles = 3
